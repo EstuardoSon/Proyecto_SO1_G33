f2a52df1df6b04a2ccacfbe1c64726ba6f5434b5
Estuardo Son <2986622050101@ingenieria.usac.edu.gt>
2023-04-12 18:34:58 -0600

    SQL

---
+++ SQL/Votaciones.sql
@@ -0,0 +1,43 @@
+drop database if exists Votaciones;
+Create database if not exists Votaciones;
+use Votaciones;
+
+create table if not exists Voto (
+    Sede int,
+    Municipio varchar(30),
+    Departamento varchar(30),
+    Papeleta varchar(10),
+    Partido varchar(10)
+);
+
+insert into Voto values (1, 'San Miguel','guatemala', 'naranja', 'UFC');
+insert into Voto values (1, 'guatemala','guatemala', 'blanca', 'UFC');
+insert into Voto values (1, 'chiquimula','chiquimula', 'blanca', 'UNE');
+
+Drop procedure if exists topDPresidente;
+DELIMITER &&
+create procedure topDPresidente()
+    begin
+        select Departamento, count(*) as cantVotos from Voto where Papeleta='blanca' group by Departamento order by cantVotos desc limit 3;
+    end &&
+DELIMITER ;
+
+Drop procedure if exists cantVotosD;
+DELIMITER &&
+create procedure cantVotosD()
+    begin
+        select Departamento, Partido, count(*) as cantVotos from Voto group by Departamento,Partido;
+    end &&
+DELIMITER ;
+
+Drop procedure if exists cantVotosM;
+DELIMITER &&
+create procedure cantVotosM()
+    begin
+        select Municipio, Partido, count(*) as cantVotos from Voto group by Municipio,Partido;
+    end &&
+DELIMITER ;
+
+call topDPresidente();
+call cantVotosD();
+call cantVotosM();
